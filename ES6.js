f6ccc8e5b7cfcc41aeef9d0b15b62cde05329393
60rzvvbj <1808078515@qq.com>
2020-11-30 07:55:58 +0800

    新增Set，Map

--- ES6.js
+++ ES6.js
@@ -124,3 +124,46 @@
 // console.log(hs);
 
 //#endregion
+
+// ——————————————————————Set,Map数据结构——————————————————————
+//#region 
+
+// Set
+
+// size属性 Set集合中元素个数
+// const s1 = new Set();
+// console.log(s1.size);
+// const s2 = new Set(['张三', '李四']);
+// console.log(s2.size);
+
+// 支持forEach函数用来遍历
+// s2.forEach(value => console.log(value));
+
+// 增删改查
+// s2.add('王五'); // 添加元素
+// s2.delete('李四'); // 删除元素
+// s2.has('张三'); // 判断元素是否为Set的成员
+// s2.clear(); // 清除所有成员
+
+
+
+// Map
+
+// 创建
+// const m1 = new Map();
+
+// 添加
+// m1.set('张三', 12);
+// m1.set('李四', 15);
+
+// 获取
+// console.log(m1.get('张三'));
+// console.log(m1.get('李四'));
+
+// 元素个数
+// console.log(m1.size);
+
+// 支持forEach遍历
+// m1.forEach(value => console.log(value));
+
+//#endregion
